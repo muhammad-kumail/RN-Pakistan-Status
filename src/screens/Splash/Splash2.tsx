4b6cdf374e416b46b4500372b8aa1a68fed5b5da
MirzaArslan123 <119283732+MirzaArslan123@users.noreply.github.com>
2023-08-15 11:25:53 +0500

    fixing fonts

--- src/screens/Splash/Splash2.tsx
+++ src/screens/Splash/Splash2.tsx
@@ -5,6 +5,7 @@ import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
+import fonts from '../../assets/fonts/fonts';
 const Splash2: React.FC<any> = ({ navigation }) => {
     // useEffect(()=>{
     //     const timer = setTimeout(() => {
@@ -16,9 +17,14 @@ const Splash2: React.FC<any> = ({ navigation }) => {
     return (
         <SafeAreaView style={{flex:1}}>
             <ImageBackground source={images.splash2Img} style={{ height: hp(100), width: wp(100) }} >
-                <TouchableOpacity>
-                    <Text style={{color:'white', fontSize:12}}>Lets Go</Text>
-                </TouchableOpacity>
+                <View style={{position:'absolute', bottom:hp(10), width:wp(100), alignItems:'center'}}>
+                    <Text style={{fontSize:wp(5), fontFamily:fonts.medium, color:"#FFFFFF"}}>Best Whatsapp Status</Text>
+                    <TouchableOpacity>
+                        <Text style={{color:'white',
+                         fontFamily:fonts.medium, 
+                         fontSize:wp(4), paddingHorizontal:wp(13), paddingVertical:wp(2.5), borderRadius:wp(5), marginTop:hp(2.5), backgroundColor:'#B036C1'}}>Let's go</Text>
+                    </TouchableOpacity>
+                </View>
             </ImageBackground>
         </SafeAreaView>
     )
